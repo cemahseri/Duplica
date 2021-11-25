28a8acd3f8d6f4daaf2f3f2eca43e85cd800c216
Yunus Emre AYGÜN <callahan_555@hotmail.com>
2021-11-25 18:37:04 +0300

    Added models.

---
+++ Models/DuplicateFileGroup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FastestDuplicateFileFinder.Models;
+
+internal sealed class DuplicateFileGroup
+{
+    internal long Size { get; set; }
+
+    internal IReadOnlyCollection<DuplicateFileInfo> Files { get; set; }
+
+    internal DuplicateFileGroup(long size, IEnumerable<DuplicateFileInfo> files)
+    {
+        Size = size;
+
+        Files = files.ToList().AsReadOnly();
+    }
+}
---
+++ Models/DuplicateFileInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace FastestDuplicateFileFinder.Models;
+
+internal sealed class DuplicateFileInfo
+{
+    internal bool IsUnique { get; set; }
+
+    internal FileInfo FileInfo { get; set; }
+
+    internal DuplicateFileInfo(FileInfo fileInfo)
+    {
+        FileInfo = fileInfo;
+    }
+}
